3d475bd05319b6f7b26bc98a54ea812e779b8883
Ethan Soo <ethansoo@ciagile.com>
2026-02-07 00:52:59 +0800

    Refactored. R9

--- src/site/components/teamworks/v2/HeroSectionV2.tsx
+++ src/site/components/teamworks/v2/HeroSectionV2.tsx
@@ -50,7 +50,7 @@ export function HeroSectionV2() {
                 <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-transform" />
               </a>
               <a 
-                href="#/teamworks/bookConsultation.html"
+                href="bookConsultation.html"
                 className="border-2 border-gray-300 text-gray-900 px-8 py-4 rounded-full hover:border-black transition-all text-center"
               >
                 Talk to Us
